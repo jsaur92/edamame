6a3f6fe6dd4596e27537c06963283e268bff1a9e
jsaur92 <6awesomejoe@gmail.com>
2026-05-19 16:38:55 -0400

    add activities block

--- scenes/ui/logic_view/click_handler.gd
+++ scenes/ui/logic_view/click_handler.gd
@@ -6,4 +6,3 @@ extends Control
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("move_click"):
 		movable_node.position += event.relative
-		print("hi")
