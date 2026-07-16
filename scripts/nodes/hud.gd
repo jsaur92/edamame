4a0c4519135d9909210ec81214ea730769ee79da
jsaur92 <6awesomejoe@gmail.com>
2026-07-16 12:20:40 -0400

    project name change and begin inventory ui update

--- scripts/nodes/hud.gd
+++ scripts/nodes/hud.gd
@@ -1,26 +1,19 @@
 class_name HUD
 extends Control
 
-@export var container : HBoxContainer
+@export var inventory_control : Control
 const ICON_SIZE : int = 192
 
 func update_items(inventory:Inventory) -> void:
-	var i : int = 0
-	while i < inventory.items.size():
-		if i < container.get_child_count():
-			container.get_child(i).visible = true
-			container.get_child(i).texture = get_icon_texture(inventory.items[i])
-		else:
-			var img = TextureRect.new()
-			img.texture = get_icon_texture(inventory.items[i])
-			container.add_child(img)
-		i += 1
-	while i < container.get_child_count():
-		container.get_child(i).visible = false
-		i += 1
+	pass
 
 
-func get_icon_texture(item:ObjectData) -> Texture2D:
-	var img = ImageTexture.create_from_image(item.get_image())
-	img.set_size_override(Vector2i(ICON_SIZE, ICON_SIZE))
-	return img
+#func get_icon_texture(item:ObjectData) -> Texture2D:
+	#var img = ImageTexture.create_from_image(item.get_image())
+	#img.set_size_override(Vector2i(ICON_SIZE, ICON_SIZE))
+	#return img
+
+
+func _on_texture_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		inventory_control.visible = not inventory_control.visible
